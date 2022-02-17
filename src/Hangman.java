f3d0f18534890c006f64e60cce0a0eb7c0e69aa8
Noah Lake <noahlake07@gmail.com>
2022-02-17 12:42:35 -0700

    Created Hangman Class
    Project started; the class now extends GraphicsProgram

---
+++ src/Hangman.java
@@ -0,0 +1,20 @@
+import acm.program.GraphicsProgram;
+
+public class Hangman extends GraphicsProgram {
+
+    @Override
+    public void init(){
+
+        
+
+    }
+
+    public Hangman(){
+
+    }
+
+    public static void main(String[] args) {
+        new Hangman().start();
+    }
+
+}
